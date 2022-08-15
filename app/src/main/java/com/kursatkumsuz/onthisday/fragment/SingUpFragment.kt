24c9ff1396298837c6e17b1165565547bb31baa7
DrHistory <kursatkumsuz@gmail.com>
2022-08-15 10:54:28 +0300

    add SignUpFragment

---
+++ app/src/main/java/com/kursatkumsuz/onthisday/fragment/SingUpFragment.kt
@@ -0,0 +1,24 @@
+package com.kursatkumsuz.onthisday.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kursatkumsuz.onthisday.R
+
+
+class SingUpFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sing_up, container, false)
+    }
+
+
+}
