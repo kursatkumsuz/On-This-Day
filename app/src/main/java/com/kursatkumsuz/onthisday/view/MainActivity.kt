2e1e32e656368d802a8c48ff952535824592f777
DrHistory <kursatkumsuz@gmail.com>
2022-08-15 10:53:11 +0300

    move to view

--- app/src/main/java/com/kursatkumsuz/onthisday/view/MainActivity.kt
+++ app/src/main/java/com/kursatkumsuz/onthisday/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kursatkumsuz.onthisday
+package com.kursatkumsuz.onthisday.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kursatkumsuz.onthisday.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
