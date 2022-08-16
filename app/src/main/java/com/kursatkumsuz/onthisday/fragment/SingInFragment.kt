9288b444cbc6f9fcf4a43d10e506cc7cb089dd42
DrHistory <kursatkumsuz@gmail.com>
2022-08-16 16:34:10 +0300

    integrate with firebase

--- app/src/main/java/com/kursatkumsuz/onthisday/fragment/SingInFragment.kt
+++ app/src/main/java/com/kursatkumsuz/onthisday/fragment/SingInFragment.kt
@@ -5,20 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kursatkumsuz.onthisday.R
 import com.kursatkumsuz.onthisday.databinding.FragmentSingInBinding
 
 
 class SingInFragment : Fragment() {
 
-    private var _binding : FragmentSingInBinding? = null
+    private var _binding: FragmentSingInBinding? = null
     private val binding get() = _binding!!
+    private var auth = Firebase.auth
+    private lateinit var email: String
+    private lateinit var password: String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       _binding = FragmentSingInBinding.inflate(layoutInflater , container , false)
+        _binding = FragmentSingInBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
@@ -26,6 +32,31 @@ class SingInFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         navigateSignUpFragment()
+        signIn()
+    }
+
+
+    private fun signIn() {
+        binding.signUpBtn.setOnClickListener { view ->
+
+            email = binding.emailText.text.toString()
+            password = binding.passwordText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                    // Navigate -> FeedFragment
+                    val action = SingInFragmentDirections.actionSingInFragmentToFeedFragment()
+                    Navigation.findNavController(view).navigate(action)
+
+                }.addOnFailureListener {
+                    Toast.makeText(context, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(context, "Please Enter Email and Password", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 
     private fun navigateSignUpFragment() {
--- app/src/main/java/com/kursatkumsuz/onthisday/fragment/SingUpFragment.kt
+++ app/src/main/java/com/kursatkumsuz/onthisday/fragment/SingUpFragment.kt
@@ -5,20 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kursatkumsuz.onthisday.R
+import com.kursatkumsuz.onthisday.databinding.FragmentSingUpBinding
 
 
 class SingUpFragment : Fragment() {
 
 
-
+    private var _binding: FragmentSingUpBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var email: String
+    private lateinit var password: String
+    private var auth = Firebase.auth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sing_up, container, false)
+        _binding = FragmentSingUpBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        signUp()
+    }
+
+    private fun signUp() {
+
+        binding.signUpBtn.setOnClickListener { view ->
+            email = binding.emailText.text.toString()
+            password = binding.passwordText.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                    Toast.makeText(context, "Register is Succesfull", Toast.LENGTH_LONG).show()
+                    //Navigate -> SignInFragment
+                    val action = SingUpFragmentDirections.actionSingUpFragmentToSingInFragment()
+                    Navigation.findNavController(view).navigate(action)
+                }.addOnFailureListener {
+                    Toast.makeText(context, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+
+            } else {
+                Toast.makeText(context, "Please Enter E-Mail and Password", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+    }
 
 }
